15370f02868d1635fb93467c01cd37816a234126
gswyhq <gswyhq@126.com>
2017-08-06 00:47:35 +0800

    增加记录h

--- linux系统相关/fanyi.py
+++ linux系统相关/fanyi.py
@@ -7,6 +7,7 @@ import json
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 import urllib.request
+import http.cookiejar
 
 # 中文-> 英文
 zh_en_url = 'https://translate.google.cn/#zh-CN/en/'
@@ -14,44 +15,84 @@ zh_en_url = 'https://translate.google.cn/#zh-CN/en/'
 # 英文-> 中文
 en_zh_url = 'https://translate.google.cn/#en/zh-CN/'
 
-# 命令行打开百度搜索使用示例：
-# gswyhq@gswyhq-pc:~$ ./baidu.py
-# gswyhq@gswyhq-pc:~$ ./baidu.py 怎么上网 python
+# 命令行翻译使用示例：
+# gswyhq@gswyhq-pc:~$ vim .bash_aliases
+# alias fanyi='python3 /home/gswyhq/hello-world/linux系统相关/fanyi.py'
+# gswyhq@gswyhq-pc:~$ fanyi python
+# gswyhq@gswyhq-pc:~$ fanyi 你好 中国
+
+def get_text(url='https://translate.google.cn/#en/zh-CN/get%0Aset'):
+    from selenium import webdriver
+    from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+    dcap = dict(DesiredCapabilities.PHANTOMJS)  # 设置userAgent
+    dcap["phantomjs.page.settings.userAgent"] = (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/25.0 ")
+    # 不加载图片
+    dcap["phantomjs.page.settings.loadImages"] = False
+
+    service_args = []
+    service_args.append('--load-images=no')  ##关闭图片加载
+    service_args.append('--disk-cache=yes')  ##开启缓存
+    service_args.append('--ignore-ssl-errors=true')  ##忽略https错误
+
+    obj = webdriver.PhantomJS(executable_path='/usr/bin/phantomjs', desired_capabilities=dcap,
+                              service_args=service_args)  # 加载网址
+
+    # 隐式等待5秒，可以自己调节
+    obj.implicitly_wait(5)
+    # 设置10秒页面超时返回，类似于requests.get()的timeout选项，driver.get()没有timeout选项
+    # 以前遇到过driver.get(url)一直不返回，但也不报错的问题，这时程序会卡住，设置超时选项能解决这个问题。
+    obj.set_page_load_timeout(10)
+    # 设置10秒脚本超时时间
+    obj.set_script_timeout(10)
+
+    obj.maximize_window()  # 设置全屏
+
+    obj.get(url)  # 打开网址
+    # obj.save_screenshot("1.png")  # 截图保存
+
+    # data = obj.page_source
+    # print(data)
+    # print(obj.title)
+    # print(obj.find_element_by_xpath('//*[@id="result_box"]'))
+    # print(obj.find_element_by_id("result_box").text)
+    text = obj.find_element_by_xpath('//*[@id="result_box"]').text
+    obj.quit()  # 关闭浏览器。当出现异常时记得在任务浏览器中关闭PhantomJS，因为会有多个PhantomJS在运行状态，影响电脑性能
+    print(text)
+    return text
+
+def open_chrome(url):
+    from selenium import webdriver
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+    browser = webdriver.Chrome(chrome_options=options)
+    # browser = webdriver.Firefox()
+
+    browser.get(url)
+    # browser.find_element_by_id("kw").send_keys("selenium")
+    # browser.find_element_by_id("su").click()
+    # time.sleep(3)  # 休眠3秒
+    browser.quit()
+
+def main():
+    if len(sys.argv) > 1:
+        argv_list = sys.argv[1:]
+    else:
+        argv_list = []
+
+    if all(re.search('^([a-zA-Z])+$', t) for t in argv_list):
+        url = en_zh_url
+    else:
+        url = zh_en_url
+
+    url = url + quote(' '.join(argv_list))
+
+    # 直接通过获取翻译结果， 但是速度太慢
+    # get_text(url)
+    # 打开浏览器
+    open_chrome(url)
 
 
-if len(sys.argv) > 1:
-    argv_list = sys.argv[1:]
-else:
-    argv_list = []
-
-if all(re.search('^([a-zA-Z])+$', t) for t in argv_list):
-    url = en_zh_url
-else:
-    url = zh_en_url
-
-url = url + quote(' '.join(argv_list))
-
-req = urllib.request.Request(url, headers={
-    'age': 0,
-    'cache-control': 'max-age=14400',
-    'Connection': 'keep-alive',
-    # 'Content-Encoding': 'gzip',
-    'Content-Type': 'text/html',
-    'Server': 'nginx',
-    # 'Transfer-Encoding':'chunked',
-    'VAR-Cache': 'HIT',
-    'Vary': 'Accept-Encoding',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, sdch',
-    'Accept-Language': 'zh-CN,zh;q=0.8',
-    'Cache-Control': 'no-cache',
-    'Pragma': 'no-cache',
-    'Upgrade-Insecure-Requests': 1,
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36'})
-
-oper = urllib.request.urlopen(req)
-data = oper.read()
-u_data = data.decode('utf8')
-soup = BeautifulSoup(data, 'lxml')
-soup.find('//*[@id="result_box"]/span')
-
+if __name__ == '__main__':
+    main()
---
+++ 爬虫/无需浏览器的模拟登陆.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+
+def test1(url='https://www.baidu.com/'):
+    from selenium import webdriver
+    # PhantomJS 是一个基于 WebKit（WebKit是一个开源的浏览器引擎，Chrome，Safari就是用的这个浏览器引擎） 的服务器端 JavaScript API，
+    # 主要应用场景是：无需浏览器的 Web 测试，页面访问自动化，屏幕捕获，网络监控
+    # sudo apt-get install PhantomJS
+    # gswyhq@gswyhq-pc:~$ whereis phantomjs
+    # phantomjs: /usr/bin/phantomjs /usr/lib/phantomjs /usr/share/man/man1/phantomjs.1.gz
+    driver = webdriver.PhantomJS(executable_path='/usr/bin/phantomjs')
+    # obj = webdriver.PhantomJS(executable_path='C:\Python27\Scripts\phantomjs.exe') #windows
+
+    # 隐式等待5秒，可以自己调节
+    driver.implicitly_wait(5)
+    # 设置10秒页面超时返回，类似于requests.get()的timeout选项，driver.get()没有timeout选项
+    # 以前遇到过driver.get(url)一直不返回，但也不报错的问题，这时程序会卡住，设置超时选项能解决这个问题。
+    driver.set_page_load_timeout(10)
+    # 设置10秒脚本超时时间
+    driver.set_script_timeout(10)
+
+    driver.maximize_window()  # 设置全屏
+
+    driver.get(url)
+
+    data = driver.page_source
+    print(data)
+    print(data.title)
+    print('京ICP证030173号' in data)
+    driver.quit()
+
+
+def test2(url='https://translate.google.cn/#en/zh-CN/get%0Aset'):
+    from selenium import webdriver
+    from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+    dcap = dict(DesiredCapabilities.PHANTOMJS)  # 设置userAgent
+    dcap["phantomjs.page.settings.userAgent"] = (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:25.0) Gecko/20100101 Firefox/25.0 ")
+    # 不加载图片
+    dcap["phantomjs.page.settings.loadImages"] = False
+
+    service_args = []
+    service_args.append('--load-images=no')  ##关闭图片加载
+    service_args.append('--disk-cache=yes')  ##开启缓存
+    service_args.append('--ignore-ssl-errors=true')  ##忽略https错误
+
+    obj = webdriver.PhantomJS(executable_path='/usr/bin/phantomjs', desired_capabilities=dcap,
+                              service_args=service_args)  # 加载网址
+
+    # 隐式等待5秒，可以自己调节
+    obj.implicitly_wait(5)
+    # 设置10秒页面超时返回，类似于requests.get()的timeout选项，driver.get()没有timeout选项
+    # 以前遇到过driver.get(url)一直不返回，但也不报错的问题，这时程序会卡住，设置超时选项能解决这个问题。
+    obj.set_page_load_timeout(10)
+    # 设置10秒脚本超时时间
+    obj.set_script_timeout(10)
+
+    obj.maximize_window()  # 设置全屏
+
+    obj.get(url)  # 打开网址
+    # obj.save_screenshot("1.png")  # 截图保存
+
+    data = obj.page_source
+    print(data)
+    print(obj.title)
+    print(obj.find_element_by_xpath('//*[@id="result_box"]'))
+    print(obj.find_element_by_id("result_box").text)
+    obj.quit()  # 关闭浏览器。当出现异常时记得在任务浏览器中关闭PhantomJS，因为会有多个PhantomJS在运行状态，影响电脑性能
+
+
+def test3():
+    from selenium import webdriver
+
+    driver = webdriver.PhantomJS()
+    driver.get('http://stackoverflow.com/')
+
+    cookies = driver.get_cookies()
+
+    driver.delete_all_cookies()
+
+    for cookie in cookies:
+        driver.add_cookie({k: cookie[k] for k in ('name', 'value', 'domain', 'path', 'expiry')})
+
+
+def main():
+    test1()
+
+
+if __name__ == '__main__':
+    main()
+
+
+# 有时，phantomJS获得的页面源码的确存在某元素，但通过find_element_by_xpath()等定位函数却无法获得该元素对象，
+# 总是提示“元素不存在”的错误。遇到这种情况，除了检查元素节点路径是否正确外，还应该分析页面源码，
+# 检查元素是否被包裹在一个特定的frame中，如果是后者，那么在使用查找函数前，需要额外的处理。
+#
+# 比如网页源码中有如下代码:
+#
+# <iframe id="topmenuFrame" width="100%" scrolling="no" height="100%" src="topmenu.aspx?>
+# <div id="haha">text</div>
+# </iframe>
+# 假如你想要获取id="haha"的div标签，直接通过driver.find_element_by_id('haha')就会提示“元素不存在“的错误。
+#
+# 这时需要使用driver.switch_to_frame(driver.find_element_by_id``("topmenuFrame"))，即先进入id为topmenuFrame的frame，
+# 然后再执行driver.find_element_by_id("haha")，就能正确获得该元素了。
+#
+# 需要注意的是，切换到这个frame之后，只能访问当前frame的内容，如果想要回到默认的内容范围，相当于默认的frame，
+# 还需要使用driver.switch_to_default_content()。
+# 页面中有多个frame时，要注意frame之间的切换。
