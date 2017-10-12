6ce842d153106995589fe8895b4663a35dd6b6c4
gswyhq <gswyhq@126.com>
2017-10-12 21:40:42 +0800

    增加记录h

---
+++ 人脸识别/face.py
@@ -0,0 +1,8 @@
+
+# https://github.com/ageitgey/face_recognition
+
+import face_recognition
+image = face_recognition.load_image_file("your_file.jpg")
+face_locations = face_recognition.face_locations(image)
+
+
