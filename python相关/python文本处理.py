a25c7f50eacc2752b8a41faca23e9fc50bccdc2b
gswyhq <gswyhq@126.com>
2019-11-12 18:46:41 +0800

    增加记录g

--- python相关/python文本处理.py
+++ python相关/python文本处理.py
@@ -4,3 +4,10 @@ import unicodedata
 unicodedata.normalize('NFKD', '，')  # 将文本标准化
 # 会将中文逗号变化英文逗号；但是不会将繁体字转换为简体字
 
+列表去重，但保持顺序不变：
+recommend_question = list('abdwqfxsfwxwqe')
+temp_recommend_question = list(set(recommend_question))
+temp_recommend_question.sort(key=recommend_question.index)
+temp_recommend_question
+Out[7]: ['a', 'b', 'd', 'w', 'q', 'f', 'x', 's', 'e']
+
