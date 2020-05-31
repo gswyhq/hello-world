166d2132ccf4fd294b53c5b4c54c9508e6da5184
gswyhq <gswyhq@126.com>
2020-05-31 16:58:40 +0800

    增加记录h

---
+++ transformers/利用bert预训练模型生成英文摘要.py
@@ -0,0 +1,32 @@
+# 完成一个摘要抽取任务。
+
+# 首先安装最新版本的transformers和一些必要的库
+#
+# !pip install transformers --upgrade
+
+import torch
+import transformers
+from transformers import BartTokenizer, BartForConditionalGeneration
+
+
+torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+LONG_BORING_TENNIS_ARTICLE = """Andy Murray  came close to giving himself some extra preparation time for his wedding next week before ensuring that he still has unfinished tennis business to attend to. The world No 4 is into the semi-finals of the Miami Open, but not before getting a scare from 21 year-old Austrian Dominic Thiem, who pushed him to4-4 in the second set before going down 3-6 6-4, 6-1 in an hour and three quarters. Murray was awaiting the winner from the last eight match between Tomas Berdych and Argentina's Juan Monaco. Prior to this tournament Thiem lost in the second round of a Challenger event to soon-to-be new Brit Aljaz Bedene. Andy Murray pumps his first after defeating Dominic Thiem to reach the Miami Open semi finals . Muray throws his sweatband into the crowd after completing a 3-6, 6-4, 6-1 victory in Florida . Murray shakes hands with Thiem who he described as a 'strongguy' after the game . And Murray has a fairly simple message for any of his fellow British tennis players who might be agitated about his imminent arrival intothe home ranks: don't complain. Instead the British No 1 believes his colleagues should use the assimilation of the world number 83, originally from Slovenia, as motivation to better themselves. At present any grumbles are happening in private, and Bedene's present ineligibility for the Davis Cup team has made it lessof an issue, although that could change if his appeal to play is allowed by theInternational Tennis Federation. Murray thinks anyone questioning the move, nowit has become official, would be better working on getting their ranking closerto his. 'If he was 500 in the world they wouldn't be that fussed about it but obviously he threatens their position a bit,' said the 27 year-old Scot. ' and he's obviously the British number two, comfortably. 'So they can complain but the best thing to do is use it in the right way and accept it for what it is, and try to use it as motivation whether they agree with it or not. He's British now sothey've just got to deal with it. Murray stretches for a return after starting his quarter final match slowly on the show court . Thiem held nothing back as heraced through the opening set, winning it 6-3 with a single break . The young Austrian is considered to be one of the hottest prospects on the ATP Tour . 'I would hope that all the guys who are below him now like James (Ward) , Kyle (Edmund) , Liam (Broady) they will use it as motivation. If he becomes eligible for Davis Cup then those guys are going to have to prove themselves. 'It can only be seen as a positive for those guys using it to try to get better. He's a good player but so are James and Kyle and Liam has improved. Aljaz is there, he's on the tour every week, the other guys aren't quite there yet.' For the first time Murray, who has an encyclopaedic knowledge of the top 100, gave his opinion of Bedene: 'He's a good player with a very good serve. He's a legitimate top 100 player,when he plays Challengers he's there or thereabouts, when he plays on the main tour he wins matches, it's not like he turns up and always loses in the first round. Murray's fiancee was once again watching from the stands shaded by a huge brimmed hat . Kim Sears flashes her enormous diamond engagement ring while watching her beau on court . 'He had a bad injury last year (wrist) but has recovered well. I would imagine he would keep moving up the rankings although I don't knowexactly how high he can go. I've practised with him a couple of times, I haven't seen him play loads, but when you serve as well as he does it helps. I would imagine he' s going to be comfortably in the top 70 or 80 in the world for a while.' It is understood the Lawn Tennis Association will give background support tohis case regarding the Davis Cup but have made it clear that the onus is on himto lead the way. An official statement said: 'To have another player in the men's top 100 is clearly a positive thing for British tennis and so we very much welcome Aljaz's change in citizenship.' The last comparable switch came twenty years ago when Greg Rusedski arrived from Canada. It was by no means universally popular but, like Bedene, he pledged that he was in for the long haul and, in fairness to him, he proved true to his word. Loising the first set shocked Murray into life as he raced to a commanding lead in the second . The No 3 seed sent overa few glaring looks towards his team before winning the second set . Murray hadto put such matters aside as he tackled the unusually talented Thiem, a delightto watch. Coached by Boris Becker's veteran mentor Gunter Bresnik, he slightly resembles Andy Roddick and hits with similar power but more elegance. His singlehanded backhand is a thing of rare beauty. However, he has had a mediocre season coming into this event and there was little to forewarn of his glorious shotmaking that seemed to catch Murray unawares early on. The world No 4 looked to have worked him out in the second, but then suffered one of his periopdic mental lapses and let him back in from 4-1 before closing it out with a break. After breaking him for 3-1 in the decider the Austrian whirlwind burnt itself out. 'He's astrong guy who hits the ball hard and it became a very physical match,' said Murray. Murray was presented with a celebratory cake after winning his 500th matchin the previous round ."""
+
+# 调用transformers库中的BART，进行摘要生成
+# tokenizer = BartTokenizer.from_pretrained('bert-base-chinese')
+# model = BartForConditionalGeneration.from_pretrained('bert-base-chinese')
+
+# transformers
+tokenizer = BartTokenizer.from_pretrained('bart-large-cnn')
+model = BartForConditionalGeneration.from_pretrained('bart-large-cnn')
+
+
+article_input_ids = tokenizer.batch_encode_plus([LONG_BORING_TENNIS_ARTICLE], return_tensors='pt', max_length=1024)['input_ids'].to(torch_device)
+summary_ids = model.generate(article_input_ids, num_beams=4, length_penalty=2.0, max_length=142, min_length=56, no_repeat_ngram_size=3)
+summary_txt = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
+print('> **Summary: **' + summary_txt)
+
+
+# 资料来源：
+# https://blog.csdn.net/hecongqing/article/details/105424563
